51749a23c1822810ab2c6fd75fd2221479d700b3
Karen Payne <Karen.1.Payne@oregon.gov>
2021-01-17 09:29:31 -0800

    mdb code sample

---
+++ Access_mdb/DataOperations.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Access_mdb
+{
+    public class DataOperations
+    {
+        public static string _connectionString = 
+            $"Provider=Microsoft.Jet.OLEDB.4.0;Data Source={Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "db1.mdb")};";
+
+        public static List<string> Read()
+        {
+            
+            var list = new List<string>();
+            
+            using (var cn = new OleDbConnection() {ConnectionString = _connectionString})
+            {
+                using (var cmd = new OleDbCommand() {Connection = cn})
+                {
+                    cmd.CommandText = "SELECT [Consonant Determiner Required] FROM DeterminersWithA;";
+                    
+                    cn.Open();
+
+                    var reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        list.Add(reader.GetString(0));
+                    }
+                }
+            }
+
+            return list;
+        }
+    }
+}
---
+++ Access_mdb/readme.md
@@ -0,0 +1,5 @@
+﻿# About
+
+Simple code sample to read from Microsoft Access .mdb into a ComboBox
+
+![screen](assets/mdb.png)
