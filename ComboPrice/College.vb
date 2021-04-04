4c7f25861d2330172802251e9a2017321b15b0b8
Karen Payne <Karen.1.Payne@oregon.gov>
2021-04-04 06:56:24 -0700

    removed two projects

--- ComboPrice/College.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class College
-    Public Property Id() As Integer
-    Public Property Name() As String
-    Public Property Credit() As Double
-
-    Public Overrides Function ToString() As String
-        Return Name
-    End Function
-End Class
